5e9ae43adc1c5c7cc164fef06485718b01ef94f7
Aynalemfan <aynalemfan@gmail.com>
2022-08-17 18:24:59 -0500

    adds application starter

---
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/HellocircleApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.hellocircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellocircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocircleApplication.class, args);
+	}
+
+}
